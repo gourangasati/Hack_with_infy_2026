a7a720200711a170be23da156f112631c27722b5
Gourang Asati <gourangasati0@gmail.com>
2026-03-17 19:53:54 +0530

    greedy updated

---
+++ greedy/dishprob.py
@@ -0,0 +1,27 @@
+from collections import defaultdict
+def food(A):
+    n = len(A)
+    pos = {}
+    max_count = 0
+    max_item = 0
+    for i, item in enumerate(A):
+        if item not in pos:
+            pos[item] = []
+        pos[item].append(i)
+    for ite in pos:
+        count = 0
+        item = pos[ite]
+        for i,val in enumerate(item):
+            if i== 0:
+                count+= 1
+                continue
+            if val-item[i-1]> 1:
+                count += 1
+        if count > max_count :
+            max_count = count
+            max_item = ite
+        if count == max_count:
+            max_item = min(max_item , ite)
+    return max_item
+A = [2,1,2,1,2,1,2,1,2]
+print(food(A))
---
+++ oops/base2.py
@@ -0,0 +1,5 @@
+print("inside b")
+import basic
+print("inside b file")
+if __name__ == "__main__":
+    print("secure")
